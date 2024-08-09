55d46bc6797c62d554ff97a6c10c6a24710685bb
mathew.brown <mathew.brown@hakai.org>
2024-08-09 11:04:20 -0700

    prep for github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
-  // Add basePath if your repository name is not your GitHub username
-  basePath: "/cogs-explore",
-  assetPrefix: "/cogs-explore/",
+  basePath: process.env.NODE_ENV === "production" ? "/cogs-explore" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/cogs-explore/" : "",
 };
